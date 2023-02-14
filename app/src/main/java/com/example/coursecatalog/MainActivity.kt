92854925a6000aef57487ea25c75529da2c8593e
hebbi4 <hebbi4@gmail.com>
2023-02-14 17:57:27 +0000

    vera með

--- app/src/main/java/com/example/coursecatalog/MainActivity.kt
+++ app/src/main/java/com/example/coursecatalog/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.coursecatalog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Hebbi er snillingur!
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
