e20bca39125457200c2ab3a8d086b606fd1aefa2
hebbi4 <hebbi4@gmail.com>
2023-03-09 16:38:29 +0000

    bjó til userActivity, það þarf samt að laga það einhvað. þetta keyrir ekki

---
+++ app/src/main/java/com/example/coursecatalog/userActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coursecatalog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class userActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
